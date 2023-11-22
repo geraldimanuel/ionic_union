aff2c5cc8b1408b93e489f457be65d2ee86008e0
Steve Christian <94883724+stevgamz@users.noreply.github.com>
2023-11-22 13:20:34 +0700

    Buttom Nav (mentah)

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,18 @@
 import {
 	IonContent,
 	IonHeader,
+	IonIcon,
+	IonLabel,
 	IonPage,
+	IonTabBar,
+	IonTabButton,
 	IonTitle,
 	IonToolbar,
 } from "@ionic/react";
 import ExploreContainer from "../components/ExploreContainer";
 import "./Home.css";
 import { Link } from "react-router-dom";
+import { bag, albums, home, calendar, people } from "ionicons/icons";
 
 const Home: React.FC = () => {
 	return (
@@ -28,6 +33,42 @@ const Home: React.FC = () => {
 					<h1>Register</h1>
 				</Link>
 			</IonContent>
+			<div className="" style={{}}>
+			<IonTabBar color="primary" slot="bottom" 
+				style={{
+					borderRadius: "20px",
+					position: "floating",
+					padding: "10px",
+					bottom: "20px",
+					marginBottom: "15px",
+				}}>
+                <IonTabButton tab="organization" href="/organization">
+                    <IonIcon icon={bag} color="light" size="small"/>
+                    <IonLabel>Organization</IonLabel>
+                </IonTabButton>
+                <IonTabButton tab="events" href="/events">
+                    <IonIcon icon={albums} color="light" size="small"/>
+                    <IonLabel>Events</IonLabel>
+                </IonTabButton>
+                <IonTabButton tab="home" href="/home">
+                    <IonIcon icon={home} size="large" style={{
+						// backgroundRadius: "30px",
+						// backgroundShape: "circle",
+						// backgroundColor: "red",
+						borderRadius: "15px",
+						
+					}} />
+                </IonTabButton>
+                <IonTabButton tab="calendar" href="/calendar">
+                    <IonIcon icon={calendar} color="light" size="small"/>
+                    <IonLabel>Calendar</IonLabel>
+                </IonTabButton>
+                <IonTabButton tab="profile" href="/profile">
+                    <IonIcon icon={people} color="light" size="small"/>
+                    <IonLabel>Profile</IonLabel>
+                </IonTabButton>
+            </IonTabBar>
+			</div>
 		</IonPage>
 	);
 };
---
+++ src/pages/Tabs.tsx
@@ -0,0 +1,14 @@
+import { IonApp, IonIcon, IonLabel, IonTabBar, IonTabButton } from "@ionic/react"
+import { albums, bag, calendar, home, people } from "ionicons/icons"
+
+
+
+const Tabs: React.FC = () => {
+    return(
+        <IonApp>
+            
+        </IonApp>
+    )
+}
+
+export default Tabs;
