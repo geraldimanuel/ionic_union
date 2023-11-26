ae68be0b501c332e2e652d10cc887cb65f95a238
bellashrns <bella.saharani@student.umn.ac.id>
2023-11-26 13:15:04 +0700

    feat: :sparkles: 85%

--- src/pages/Organization.tsx
+++ src/pages/Organization.tsx
@@ -8,72 +8,97 @@ const Organization: React.FC = () => {
             </IonHeader>
             <IonContent className="ion-padding">
                 <h2>My Organization</h2>
-                {/* <IonGrid>
-                    <IonRow>
-                        <IonCol>
-                            <IonCard className="ion-text-center" style={{ height: "78px" }}>
-                                <IonGrid>
-                                    <IonRow>
-                                        <IonCol>
-                                            <img src="./imkom.png" />
-                                        </IonCol>
-                                        <IonCol>
-                                            <h3>Im'Kom</h3>
-                                        </IonCol>
-                                    </IonRow>
-                                </IonGrid>
-                            </IonCard>
-                        </IonCol>
-                        <IonCol>
-                            <IonCard style={{ height: "78px" }}>
-                                <IonGrid>
-                                    <IonRow>
-                                        <IonCol>
-                                            <img style={{ marginTop: "20px" }} src="./radio.png" />
-                                        </IonCol>
-                                        <IonCol>
-                                            <h3>UMN Radio</h3>
-                                        </IonCol>
-                                    </IonRow>
-                                </IonGrid>
-                            </IonCard>
-                        </IonCol>
-                    </IonRow>
-                </IonGrid> */}
-
-                <IonContent className="wrapper" style={{ height: "120px", overflowX: "auto", whiteSpace: "nowrap" }}>
-                    <div style={{ display: "flex" }}> 
-                        <IonCard className="ion-text-center" style={{ height: "78px", width: "180px" }}>
-                            <IonGrid>
-                                <IonRow>
-                                    <IonCol>
-                                        <img style={{ marginTop: "3px" }} src="./imkom.png" />
-                                    </IonCol>
-                                    <IonCol size="8">
-                                        <h3>Im'Kom</h3>
-                                    </IonCol>
-                                </IonRow>
-                            </IonGrid>
-                        </IonCard>
-
-                        <IonCard className="ion-text-center" style={{ height: "78px", width: "180px" }}>
-                            <IonGrid>
-                                <IonRow>
-                                    <IonCol>
-                                        <img style={{ marginTop: "20px" }} src="./radio.png" />
-                                    </IonCol>
-                                    <IonCol size="8">
-                                        <h3>UMN Radio</h3>
-                                    </IonCol>
-                                </IonRow>
-                            </IonGrid>
-                        </IonCard>
-                    </div>
-                    
-                </IonContent>
+                <IonItem lines="none" className="orgWrapper">
+                    <IonItem className="orgItem">
+                        <IonGrid className="orgGrid">
+                            <IonRow>
+                                <IonCol>
+                                    <img style={{ marginTop: "6px" }} src="./imkom.png" />
+                                </IonCol>
+                                <IonCol size="8">
+                                    <h3>Im'Kom</h3>
+                                </IonCol>
+                            </IonRow>
+                        </IonGrid>
+                    </IonItem>
+                    <IonItem className="orgItem">
+                        <IonGrid className="orgGrid">
+                            <IonRow>
+                                <IonCol>
+                                    <img style={{ marginTop: "20px" }} src="./radio.png" />
+                                </IonCol>
+                                <IonCol size="8">
+                                    <h3>UMN Radio</h3>
+                                </IonCol>
+                            </IonRow>
+                        </IonGrid>
+                    </IonItem>
+                    <IonItem className="orgItem">
+                        <IonGrid className="orgGrid">
+                            <IonRow>
+                                <IonCol>
+                                    <img style={{ marginTop: "6px" }} src="./imkom.png" />
+                                </IonCol>
+                                <IonCol size="8">
+                                    <h3>Im'Kom</h3>
+                                </IonCol>
+                            </IonRow>
+                        </IonGrid>
+                    </IonItem>
+                    <IonItem className="orgItem">
+                        <IonGrid className="orgGrid">
+                            <IonRow>
+                                <IonCol>
+                                    <img style={{ marginTop: "20px" }} src="./radio.png" />
+                                </IonCol>
+                                <IonCol size="8">
+                                    <h3>UMN Radio</h3>
+                                </IonCol>
+                            </IonRow>
+                        </IonGrid>
+                    </IonItem>
+                </IonItem>
                 
                 <h2>Organizations</h2>
+                <IonItem lines="none" className="catWrapper">
+                    <IonItem lines="none" color="secondary" className="catItem">
+                        <small className="catText">All</small>
+                    </IonItem>
+                    <IonItem lines="none" className="catItem">
+                        <small className="catText">Himpunan</small>
+                    </IonItem>
+                    <IonItem lines="none" className="catItem">
+                        <small className="catText">Media Kampus</small>
+                    </IonItem>
+                    <IonItem lines="none" className="catItem">
+                        <small className="catText">UKM Olahraga</small>
+                    </IonItem>
+                    <IonItem lines="none" className="catItem">
+                        <small className="catText">UKM Seni</small>
+                    </IonItem>
+                </IonItem>
+
                 <IonGrid>
+                    <IonCard style={{ height: "125px"}}>
+                        <IonRow className="ion-text-center" style={{ marginTop: "30px" }}>
+                            <IonCol size="4">
+                                <img src="./imkom.png" />
+                            </IonCol>
+                            <IonCol size="4">
+                                <h3>Im'Kom</h3>
+                            </IonCol>
+                        </IonRow>
+                    </IonCard>
+                    <IonCard style={{ height: "125px" }}>
+                        <IonRow className="ion-text-center" style={{ marginTop: "30px" }}>
+                            <IonCol size="4">
+                                <img style={{ marginTop: "15px" }} src="./radio.png" />
+                            </IonCol>
+                            <IonCol size="4">
+                                <h3>UMN Radio</h3>
+                            </IonCol>
+                        </IonRow>
+                    </IonCard>
                     <IonCard style={{ height: "125px"}}>
                         <IonRow className="ion-text-center" style={{ marginTop: "30px" }}>
                             <IonCol size="4">
