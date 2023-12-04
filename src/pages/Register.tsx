97adfdbddf83229c3ba1dc564234e2d914103aab
Steve Christian <94883724+stevgamz@users.noreply.github.com>
2023-12-04 20:55:21 +0700

    .

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -2,10 +2,7 @@ import {
 	IonButton,
 	IonContent,
 	IonHeader,
-	IonIcon,
 	IonInput,
-	IonItem,
-	IonLabel,
 	IonPage,
 	IonTitle,
 	IonToolbar,
@@ -13,7 +10,6 @@ import {
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { loginWithGooglePopup, registerUser } from "../firebaseConfig";
-import { logoGoogle } from "ionicons/icons";
 
 const Register: React.FC = () => {
 	const [username, setUsername] = useState("");
@@ -38,8 +34,9 @@ const Register: React.FC = () => {
 
 	return (
 		<IonPage>
-			<IonContent>
-				{/* <h1>Register</h1>
+			<IonHeader></IonHeader>
+			<IonContent className="ion-padding">
+				<h1>Register</h1>
 				<IonInput
 					placeholder="Email"
 					onIonChange={(e: any) => setUsername(e.target.value)}
@@ -60,81 +57,7 @@ const Register: React.FC = () => {
 					<Link to={"/login"}> Login</Link>
 				</p>
 
-				<button onClick={loginWithGooglePopup}>Sign in With Google</button> */}
-
-				<div style={{ background: "linear-gradient(180deg, rgba(18,84,136,1) 0%, rgba(42,147,213,1) 100%)", height: "100%" }}>
-					<div
-						style={{
-							borderRadius: "40px 40px 0px 0px",
-							backgroundColor: "#ffffff",
-							position: "absolute",
-							bottom: "0px",
-							padding: "20px 30px 35px",
-							display: "flex",
-							flexDirection: "column",
-							minHeight: "55%",
-							justifyContent: "space-around",
-							minWidth: "100%",
-						}}
-					>
-						<h1 style={{ fontSize: "43px", marginBottom: "20px" }}>Register</h1>
-						<div style={{ marginBottom: "50px" }}>
-							<IonItem>
-								<IonLabel position="floating">E-mail</IonLabel>
-								<IonInput
-									placeholder="E-mail"
-									onIonChange={(e: any) => setUsername(e.target.value)}
-								/>
-							</IonItem>
-							<IonItem>
-								<IonLabel position="floating">Password</IonLabel>
-								<IonInput
-									placeholder="Password"
-									onIonChange={(e: any) => setPassword(e.target.value)}
-									type="password"
-								/>
-							</IonItem>
-							<IonItem>
-								<IonLabel position="floating">Confirm password</IonLabel>
-								<IonInput
-									placeholder="Re-type password"
-									onIonChange={(e: any) => setPassword(e.target.value)}
-									type="password"
-								/>
-							</IonItem>
-						</div>
-
-						<div
-							style={{
-								display: "flex",
-								flexDirection: "column",
-								gap: "10px",
-							}}
-						>
-							<div
-								style={{
-									backgroundColor: "#125488",
-									color: "#FFFFFF",
-									padding: "15px",
-									borderRadius: "20px",
-									textAlign: "center",
-									fontWeight: "bold",
-								}}
-								onClick={register}
-							>
-								<p
-									style={{
-										padding: "0px",
-										margin: "0px",
-										fontSize: "14px",
-									}}
-								>
-									Register now
-								</p>
-							</div>
-						</div>
-					</div>
-				</div>
+				<button onClick={loginWithGooglePopup}>Sign in With Google</button>
 			</IonContent>
 		</IonPage>
 	);
