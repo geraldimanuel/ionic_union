058471b4988948f82bd02ff2641f64a261052f92
Steve Christian <94883724+stevgamz@users.noreply.github.com>
2023-12-16 14:56:15 +0700

    fix: UI Calendar

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -98,16 +98,17 @@ const Calendar: React.FC = () => {
                     <p>Hello, Kesya!</p>
                     <h1 style={{
                         fontSize:"32px"
-                    }}>Let's find your <br></br> favorite events!</h1>
+                    }}>Let's find 
+                    your timeline on calendar!</h1>
                 </IonText>
-                <IonItem style={{top:"30px", borderRadius:"28px", height:"56px", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)"}}>
+                {/* <IonItem style={{top:"30px", borderRadius:"28px", height:"56px", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)"}}>
                     <IonLabel><IonIcon icon={searchOutline} /></IonLabel>
                     <IonInput 
                         style={{left:"5px", borderRadius:"28px", height:"56px"}}
                         type="text"
                         placeholder="Search here. . ."
                     />
-                </IonItem>
+                </IonItem> */}
             </div>
 
             <IonContent className="ion-padding">
